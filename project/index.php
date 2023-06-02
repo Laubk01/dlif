0b92163c8df9cda05d8f57d3f1e03d8283f6fa62
Laubk01 <114838073+Laubk01@users.noreply.github.com>
2023-06-01 20:32:35 -0600

    Cambios QR

--- project/index.php
+++ project/index.php
@@ -40,14 +40,16 @@ if(isset($_POST['add_to_cart'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Inicio</title>
+   <title>Inicio..</title>
 
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
    <link rel="stylesheet" href="css/estilo2.css">
 
+
+
 </head>
 <body>
-   
+
 <?php include 'header.php'; ?>
 
 <section class="home">
